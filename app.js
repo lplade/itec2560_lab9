c63a0529c92176d9d9ad23cfa95d4fc830651352
Larry Lade <lplade@users.noreply.github.com>
2016-03-16 14:52:57 -0500

    Initial code

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var MongoClient = require("mongodb").MongoClient;
+var engines = require("jade");
+var assert = require('assert');
+
+app express();
+
+app.set("view engine"), "jade");
+app.set("views", __dirname + "/views");
+
+//Attempt to connect to mongodb
+MongoClient.connect("mongodb://localhost:27017/garden", function(err, db){
+  assert.equal(null, err); //This will crash the app if there is an error.
+  console.log("Connected to MongoDB");
+
+  //routes - this is for the home page
+  app.get('/', function(req, res){
+    db.collection('flowers').find({}, {"name": true, "color": true}).toArray(function(err, docs){
+        if (err) { return res.sendStatus(500); }
+        return res.render('allflowers', {'flowers': docs});
+      });
+    });
+
+
+  //All other requests, return 404 not found
+  app.use(function(req, res){
+    res.sendStatus(404);
+  });
+
+  //And start the server on any port you like.
+  var server = app.listen(3050, function(){
+    var port = server.address().port;
+    console.log("Server listening on port " + port);
+  });
+
+});
